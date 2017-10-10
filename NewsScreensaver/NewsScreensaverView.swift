3a7e7c482b62b7a0fd3d7f3b21753d30bfd0911f
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-10-10 20:10:19 +0900

    Place ContentView on the screensaver

--- NewsScreensaver/NewsScreensaverView.swift
+++ NewsScreensaver/NewsScreensaverView.swift
@@ -11,6 +11,26 @@ import ScreenSaver
 
 class NewsScreensaverView: ScreenSaverView {
   
+  let contentView: ContentView = {
+    let view = ContentView.instantiate()
+    view.autoresizingMask = [ NSAutoresizingMaskOptions.viewWidthSizable, .viewHeightSizable ]
+    
+    return view
+  }()
+  
+  // MARK: - Lifecycle
+  override init?(frame: NSRect, isPreview: Bool) {
+    super.init(frame: frame, isPreview: isPreview)
+    
+    contentView.frame = bounds
+    addSubview(contentView)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - ScreenSaverView
   override var animationTimeInterval: TimeInterval {
     set {}
     get {
