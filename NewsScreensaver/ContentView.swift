839ad6acb8b7d0bb767683022f9577d615d162bf
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-10-10 20:05:19 +0900

    Add ContentView class

---
+++ NewsScreensaver/ContentView.swift
@@ -0,0 +1,12 @@
+//
+//  ContentView.swift
+//  NewsScreensaver
+//
+//  Created by mitsuyoshi.yamazaki on 2017/10/10.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import Cocoa
+
+final class ContentView: NSView, IBInstantiatable {
+}
